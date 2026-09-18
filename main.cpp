47729705a557b4f30127b1b7aec59fbb09e8a97f
Beling <julienbeling2007@gmail.com>
2026-09-18 06:59:25 +0100

    implementation de l'addition de 2 nombres

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int a;
+    int b; 
+    int c;
+    std::cout <<"entrez un nombre"<<std::endl;
+    std::cin>> a;
+    std::cout <<"entrez un nombre"<<std::endl;
+    std::cin>> b;
+
+    c= a+b;
+
+    std::cout<<c;
+}

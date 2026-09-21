8e0685cc37d5b71ba23f512b9e5f7cdab209ab95
Beling <julienbeling2007@gmail.com>
2026-09-21 11:45:57 +0100

    extension

--- main.cpp
+++ main.cpp
@@ -4,11 +4,7 @@ int main(){
     int a;
     int b; 
     int c;
-<<<<<<< HEAD
     std::cout <<"entrez un nombre decimal"<<std::endl;
-=======
-    std::cout <<"entrez un nombre entier"<<std::endl;
->>>>>>> a507cd48285dd80d8b2cbe8a47105d3133626441
     std::cin>> a;
     std::cout <<"entrez un nombre"<<std::endl;
     std::cin>> b;
